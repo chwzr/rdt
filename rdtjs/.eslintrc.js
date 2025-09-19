b2221c757c59ba66068cd190684cf25a16c3da7b
Sebastian Kürten <sebastian.kuerten@cavorit.de>
2025-09-19 08:54:54 +0200

    Format JS source code
    Run `npx prettier --write .`

--- rdtjs/.eslintrc.js
+++ rdtjs/.eslintrc.js
@@ -1,23 +1,20 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    extends: [
-        'eslint:recommended',
-        '@typescript-eslint/recommended',
-    ],
-    env: {
-        browser: true,
-        es2020: true,
-        node: true,
-    },
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-    },
-    rules: {
-        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-        '@typescript-eslint/no-explicit-any': 'warn',
-        '@typescript-eslint/ban-ts-comment': 'warn',
-    },
-}; 
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "@typescript-eslint/recommended"],
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/ban-ts-comment": "warn",
+  },
+};
--- rdtjs/jest.config.js
+++ rdtjs/jest.config.js
@@ -1,18 +1,14 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    roots: ['<rootDir>/src'],
-    testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
-    transform: {
-        '^.+\\.ts$': 'ts-jest',
-    },
-    collectCoverageFrom: [
-        'src/**/*.ts',
-        '!src/**/*.d.ts',
-        '!src/examples/**',
-    ],
-    moduleNameMapping: {
-        '^@/(.*)$': '<rootDir>/src/$1',
-    },
-    setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
-}; 
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  roots: ["<rootDir>/src"],
+  testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts", "!src/examples/**"],
+  moduleNameMapping: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/src/__tests__/setup.ts"],
+};
